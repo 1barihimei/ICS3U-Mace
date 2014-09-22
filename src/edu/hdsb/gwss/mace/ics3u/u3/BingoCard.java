a5a22928dbc4d9be5081a9f051022474ee762ba6
meisambarihi <meisambarihi@199.235.249.118>
2014-09-22 11:18:49 -0400

    Finished BingoCard

---
+++ src/edu/hdsb/gwss/mace/ics3u/u3/BingoCard.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.mace.ics3u.u3;
+
+/**
+ *
+ * @author meisambarihi
+ */
+public class BingoCard {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+ System.out.format("%10s %10s %10s %10s %10s", "B", "I", "N", "G", "O \n");
+ System.out.format("%10s %10s %10s %10s %10s" , "1", "20" , "43" , "51", "67 \n");
+ System.out.format("%10s %10s %10s %10s %10s", "4", "25", "42", "56", "69 \n");
+ System.out.format("%10s %10s %10s %10s %10s", "8", "23", "47", "57", "64 \n");
+ System.out.format("%10s %10s %10s %10s %10s", "3", "21", "FREE", "63", "74 \n");
+ System.out.format("%10s %10s %10s %10s %10s", "15", "29", "44", "59", "70 \n");
+        
+        
+    }
+    
+}
---
+++ src/edu/hdsb/gwss/mace/ics3u/u3/HelloWorld.java
@@ -0,0 +1,32 @@
+/*
+ * Name:    Mace Barihi
+ * Date:    Sept. 18, 2014
+ * Version: v0
+ * Description:
+ *  This code prints "Hello World" to the output window.
+ */
+
+package edu.hdsb.gwss.mace.ics3u.u3;
+
+/**
+ *^
+ * @author meisambarihi
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+    System.out.format("%-20s | %5s | %7s | %6s", "Team Name", "Wins", "Losses", "Draws \n \n");
+    System.out.println("----------------------------------------------");
+    System.out.format("%-20s | %5s | %7s | %6s", "Toronto" , "20" , "2" , "2\n");
+ System.out.format("%-20s | %5s | %7s | %6s", "Chicago" , "12" , "7" , "4\n");
+ System.out.format("%-20s | %5s | %7s | %6s", "Boston" , "14" , "5" , "2\n");
+ System.out.format("%-20s | %5s | %7s | %6s", "New York" , "16" , "11" , "1\n");
+ System.out.format("%-20s | %5s | %7s | %6s", "Brooklyn" , "5" , "18" , "0\n");
+    }
+    
+}
