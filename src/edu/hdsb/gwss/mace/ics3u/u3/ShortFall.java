5bdd7732bf791d6fe7988b0d34ebbf993a840285
meisambarihi <meisambarihi@199.235.249.71>
2014-09-23 14:42:26 -0400

    Started ShortFall

---
+++ src/edu/hdsb/gwss/mace/ics3u/u3/ShortFall.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.mace.ics3u.u3;
+
+/**
+ *
+ * @author meisambarihi
+ */
+public class ShortFall {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        short value = 32;
+        System.out.println("A short: " + value);
+
+    }
+
+}
