73bb2f4a30fbbaf1f75e0fb580b8927fb76fbfda
meisambarihi <meisambarihi@199.235.249.72>
2014-09-24 11:11:28 -0400

    Started Arithmetic

---
+++ src/edu/hdsb/gwss/mace/ics3u/u3/Arithmetic.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.mace.ics3u.u3;
+
+/**
+ *
+ * @author meisambarihi
+ */
+public class Arithmetic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        int a = 6;
+        int b = 3;
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b ;
+        int quotient = a / b;
+       
+
+        
+        System.out.println("the value of 'a' is " + a);
+        System.out.println("the value of 'b' is " + b);
+        System.out.println("a + b is " + sum);
+        System.out.println("a - b " + difference);
+        System.out.println("a X b " + product);
+        System.out.println("a / b " + quotient);
+    }
+
+}
