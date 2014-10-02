db9e2c2360bac7dfadff68789ec253a9f1001336
meisambarihi <meisambarihi@199.235.249.90>
2014-10-02 09:50:19 -0400

    Started OrderChecker

---
+++ src/OrderChecker.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author meisambarihi
+ */
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class OrderChecker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        // CONSTANTS
+        double boltCost = 0.05;
+        double nutCost = 0.03;
+        double washerCost = 0.01;
+
+        // VARIABLES
+        double washersNumber, boltNumber, nutsNumber, total;
+        boolean valid = true;
+
+        // OBJECTS
+        Scanner input = new Scanner(System.in);
+        NumberFormat moneyFormat = NumberFormat.getCurrencyInstance();
+
+        // INPUT; Ask, Get
+        System.out.print("Total number of BOLTS   : ");
+        boltNumber = input.nextInt();
+        System.out.print("Total number of NUTS    : ");
+        nutsNumber = input.nextInt();
+        System.out.print("Total number of WASHERS : ");
+        washersNumber = input.nextInt();
+
+    }
+
+}
