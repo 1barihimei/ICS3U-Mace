2a8fd3dfbf5ab92f7b70ad82c3eec3c5fa25b25a
meisambarihi <meisambarihi@192.168.1.10>
2014-10-09 22:22:34 -0400

    Finished BodyMassIndex

--- src/BodyMassIndex.java
+++ src/BodyMassIndex.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 /*
@@ -17,14 +16,14 @@ public class BodyMassIndex {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    // CONSTANTS 
-       
+        // CONSTANTS 
+
         // VARIABLES
         double weightLbs;
         double weightKgs = 0;
         double heightCm = 0;
         double heightInches;
-        double bmi = weightKgs/((heightCm/100) * (heightCm/100)) ;
+        double bmi;
 
 // OBJECTS
         Scanner input = new Scanner(System.in);
@@ -32,23 +31,23 @@ public class BodyMassIndex {
 // INPUT; Ask, Get
         System.out.print("BMI Mass Index Calculator\n\nBody mass index (BMI) is a measure of the weight of a person scaled according to their height.\nBMI is defined as the individual's body weight divided by the square of their height.\nIt is almost always expressed in the unit kg/m2.\n\n");
         System.out.print("You can enter height in either Inches or Centimeters.\nInput '0' for Centimeters to use Inches:\n\nHeight Centimeter  : ");
-    heightCm = input.nextInt();
-    if (heightCm == 0){
-         System.out.print("Height Centimeters  : ");
-         heightInches = input.nextInt();
-         heightCm = heightInches / 2.54;
+        heightCm = input.nextInt();
+        if (heightCm == 0) {
+            System.out.print("Height Inches  : ");
+            heightInches = input.nextInt();
+            heightCm = heightInches / 2.54;
+
         }
-    System.out.print("\nYou can enter weight in either Kilograms or Pounds.\nInput '0' for Kilograms to use Pounds:\n\nWeight Kilograms  : ");
-    weightKgs = input.nextInt();
-    if (weightKgs == 0){
-         System.out.print("weightLbs  : ");
-         weightLbs = input.nextInt();
-         weightKgs = weightLbs * 2.2046226;
+        System.out.print("\nYou can enter weight in either Kilograms or Pounds.\nInput '0' for Kilograms to use Pounds:\n\nWeight Kilograms  : ");
+        weightKgs = input.nextInt();
+        if (weightKgs == 0) {
+            System.out.print("Weight Pounds  : ");
+            weightLbs = input.nextInt();
+            weightKgs = weightLbs * 2.2046226;
+        }
+        bmi = weightKgs / ((heightCm / 100) * (heightCm / 100));
+        System.out.print("\n\nYour BMI Mass Index is  : " + bmi + " kg/m²\n");
+
     }
-        System.out.print("\n\nYour BMI Mass Index is  : " + bmi);
-        
-    
-    }
-    
-    
+
 }
