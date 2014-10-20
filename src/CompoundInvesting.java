ef123bb6cd01af5a22fb6970b29b8df323179b46
meisambarihi <meisambarihi@199.235.249.137>
2014-10-20 11:05:52 -0400

    Finished OunceGramsGOOD and Started CompoundInvesting

---
+++ src/CompoundInvesting.java
@@ -0,0 +1,22 @@
+/*
+ * Name:    Mace Barihi
+ * Date:    Oct. 20, 2014
+ * Version: v0
+ * Description:
+ *  This program asks the user to enter the number of years (up to 15 years),
+ *  the amount invested per year, and interest rate and creates a total for each year and displays a chart.
+ */
+
+public class CompoundInvesting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    System.out.print("Compound Investing \n\nThis program will print out a little table that will display the \namount of a yearly investment over a period of up to 15 years.\n");
+    
+    }
+    
+}
--- src/OunceGramsGOOD.java
+++ src/OunceGramsGOOD.java
@@ -6,7 +6,6 @@
  *  This code prints numbers 1 to 15 on the left which represent weight in ounces and converts the unit to grams
  *  which are located on the right.
  */
-
 public class OunceGramsGOOD {
 
     /**
@@ -14,17 +13,19 @@ public class OunceGramsGOOD {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+// CONSTANTS
         int oz = 1;    //* Initialized that each ounce is worth 1
 
+// INPUT; Ask, Get
         //* each oz is 28.35 grams, until oz is less than 16 the statement will run
-        for (double g = 28.35; oz < 16;) {       
+        for (double g = 28.35; oz < 16;) {
             for (; oz < 16; oz++) {     //* until oz is less than 16, oz go up by 1 looping iterate
-                
+
                 /* formatted so grams only has two decimal places, 
                  * a tab between the ounces and grams and new line
-                */
-                System.out.format("%7s \t %4.2f\n", oz, g); 
-                
+                 */
+                System.out.format("%7s \t %4.2f\n", oz, g);
+
                 g = g + 28.35;  //* grams goes up by 28.35 each time
             }
         }
