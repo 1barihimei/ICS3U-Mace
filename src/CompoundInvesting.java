6a6410c7be349f5485cab3c597452e5f7dbe385c
meisambarihi <meisambarihi@199.235.249.15>
2014-10-21 19:47:12 -0400

    Finished CompoundInvesting

--- src/CompoundInvesting.java
+++ src/CompoundInvesting.java
@@ -14,15 +14,14 @@ public class CompoundInvesting {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-
-        // CONSTANTS
 
         // VARIABLES
-        double investment =0;
+        double investment;
         double interestRate;
         int years;
         double total;
+        double amount;
+        double interest;
 
         // OBJECTS
         Scanner input = new Scanner(System.in);
@@ -36,14 +35,14 @@ public class CompoundInvesting {
         System.out.print("Enter the number of years :\t");
         years = input.nextInt();
         System.out.format("%-15s %15s %15s %13s\n", "\nYear", "Amount in Account", "Interest", "Total");
-     
-        double amount = investment;
-     for (int yearCount = 1 ; yearCount <= years; yearCount++) {
-         total = (interestRate/100)*amount+amount;   
-         double interest = amount*(interestRate/100) ;
-         System.out.format("%-15s     $%5.2f \t\t$%1.2f      \t$%5.2f\n", yearCount, amount , interest , total );    
-         amount = total + investment;
-            
-}
-}
+        amount = investment;
+
+        for (int yearCount = 1; yearCount <= years; yearCount++) {
+            total = (interestRate / 100) * amount + amount;
+            interest = amount * (interestRate / 100);
+            System.out.format("%-15s     $%5.2f \t\t$%1.2f      \t$%5.2f\n", yearCount, amount, interest, total);
+            amount = total + investment;
+
+        }
     }
+}
