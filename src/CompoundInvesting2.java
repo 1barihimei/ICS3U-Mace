e797a9654947deb075aa6bda92f4f0f739cc4793
meisambarihi <meisambarihi@199.235.249.15>
2014-10-21 19:33:37 -0400

    Finished CompoundInvesting

--- src/CompoundInvesting2.java
+++ src/CompoundInvesting2.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  *  This program asks the user to enter the number of years (up to 15 years),
  *  the amount invested per year, and interest rate and creates a total for each year and displays a chart.
  */
-public class CompoundInvesting {
+public class CompoundInvesting2 {
 
     /**
      * @param args the command line arguments
@@ -19,7 +19,7 @@ public class CompoundInvesting {
         // CONSTANTS
 
         // VARIABLES
-        double investment =0;
+        double yearlyInvestment =0;
         double interestRate;
         int years;
         double total;
@@ -28,22 +28,24 @@ public class CompoundInvesting {
         Scanner input = new Scanner(System.in);
 
         // INPUT; Ask, Get
-        System.out.print("Compound Investing \n\nThis program will print out a little table that will display the \namount of a yearly investment over a period of up to 15 years.\n\n");
-        System.out.print("Enter the yearly investment :\t");
-        investment = input.nextDouble();
+        System.out.print("Compound Investing \n\nThis program will print out a little table that will display the \namount of a yearly Investment over a period of up to 15 years.\n\n");
+        System.out.print("Enter the yearly Investment :\t");
+        yearlyInvestment = input.nextDouble();
         System.out.print("Enter the interest rate (%) :\t");
         interestRate = input.nextDouble();
         System.out.print("Enter the number of years :\t");
         years = input.nextInt();
         System.out.format("%-15s %15s %15s %13s\n", "\nYear", "Amount in Account", "Interest", "Total");
      
-        double amount = investment;
-     for (int yearCount = 1 ; yearCount <= years; yearCount++) {
-         total = (interestRate/100)*amount+amount;   
-         double interest = amount*(interestRate/100) ;
-         System.out.format("%-15s     $%5.2f \t\t$%1.2f      \t$%5.2f\n", yearCount, amount , interest , total );    
-         amount = total + investment;
-            
+        total = yearlyInvestment + interestRate;
+        System.out.format("%-15s     $%5.2f \t\t$%1.2f      \t$%5.2f\n", 1, yearlyInvestment , interestRate , total );
+        
+     for (int yearCount = 2 ; yearCount <= years; yearCount++) {
+         total = (interestRate/100)*total+total;   
+         
+            interestRate = total*(interestRate/100) ;
+        System.out.format("%-15s     $%5.2f \t\t$%1.2f      \t$%5.2f\n", yearCount, 5.8 , interestRate , total );    
+            total = total + yearlyInvestment;
 }
 }
     }
