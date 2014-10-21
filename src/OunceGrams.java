3f883d1347fb8bb78f20a770af64f29d89309468
meisambarihi <meisambarihi@199.235.249.15>
2014-10-21 10:03:39 -0400

    Chnaged OunceGrams good copy name to OunceGrams

--- src/OunceGrams.java
+++ src/OunceGrams.java
@@ -1,11 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author meisambarihi
+ * Name:    Mace Barihi
+ * Date:    Oct. 20, 2014
+ * Version: v1
+ * Description:
+ *  This code prints numbers 1 to 15 on the left which represent weight in ounces and converts the unit to grams
+ *  which are located on the right.
  */
 public class OunceGrams {
 
@@ -14,14 +13,21 @@ public class OunceGrams {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int oz = 1;
-        double g = 28.35;
-        while (oz <= 15) {
-            System.out.print(oz + "\t" + g + "\n");
-            oz++;
-            g = g + 28.35;
+// CONSTANTS
+        int oz = 1;    //* Initialized that each ounce is worth 1
+
+// INPUT; Ask, Get
+        //* each oz is 28.35 grams, until oz is less than 16 the statement will run
+        for (double g = 28.35; oz < 16;) {
+            for (; oz < 16; oz++) {     //* until oz is less than 16, oz go up by 1 looping iterate
+
+                /* formatted so grams only has two decimal places, 
+                 * a tab between the ounces and grams and new line
+                 */
+                System.out.format("%7s \t %4.2f\n", oz, g);
+
+                g = g + 28.35;  //* grams goes up by 28.35 each time
+            }
         }
-
     }
-
 }
