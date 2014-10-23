9cbb29a60d6b3f4b84a7f3aa1ffe0b8b1147b551
meisambarihi <meisambarihi@10.96.176.221>
2014-10-23 14:33:25 -0400

    Finished FibonacciSeries

--- src/FibonacciSequence.java
+++ src/FibonacciSequence.java
@@ -11,17 +11,18 @@ public class FibonacciSequence {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
 
         // VARIABLES
-        int lastNumber = 0;
+        int lastNumber = 0; //Last number initialized and kept at 0 so number 1 of the series is repeated
 
         // INPUT; Ask, Get
+        //Introduction
         System.out.print("Fibonacci Series\n\nThis program will print the first twenty terms of the Fibonacci Series.\n\n");
-        for (int currentNumber = 1; currentNumber <= 10000;) {
-            System.out.print(currentNumber + ", ");
-            currentNumber = lastNumber + currentNumber;
-            lastNumber = currentNumber - lastNumber;
+
+        for (int currentNumber = 1; currentNumber <= 10000;) {  //currentNumber which is 1 is introduced, as long as the currentNumber is less than or equal to 10000 (before the 20th number in the series)...
+            System.out.print(currentNumber + ", ");             //The currentNumber and a comma and space are outputted
+            currentNumber = lastNumber + currentNumber;         //currentNumber is added to the lastNumber
+            lastNumber = currentNumber - lastNumber;            //lastNumber becomes the currentNumber minus the lastNumber so it doesnt equal the actual current number, but the previous number
         }
     }
 
