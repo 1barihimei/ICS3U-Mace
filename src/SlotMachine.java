0754bdabe17698d86cc1643fd4ea517d3b0afd3c
meisambarihi <meisambarihi@10.96.178.218>
2014-10-24 11:11:27 -0400

    Started SlotMachine

---
+++ src/SlotMachine.java
@@ -0,0 +1,30 @@
+/*
+ * Name:    Mace Barihi
+ * Date:    Oct. 23, 2014
+ * Version: v0
+ * Description:
+ *  
+ */
+
+public class SlotMachine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // CONSTANTS
+        
+        //VARIABLES
+        int coins = 50;
+        int slot1, slot2, slot3 = (int) (Math.random() * 4) + 1;
+        
+        // OBJECTS
+        
+        
+        // INPUT; Ask, Get
+        
+        
+    }
+    
+}
